62249f535ff5a04429ce24849edd475e33cacdd5
“Kimberly <kimberly.achkio@gmail.com>
2019-02-08 07:38:25 -0700

    Complete bonus wheel tests

--- src/scripts/BonusWheel.js
+++ src/scripts/BonusWheel.js
@@ -2,21 +2,23 @@ import Wheel from './Wheel.js'
 import data from '../data.js';
 import helper from './helper.js'
 
+beforeEach(function () {
+  this.wheel = new BonusWheel({});
+});
+
 class BonusWheel extends Wheel {
   constructor() {
     super();
     this.increasedCoins = 0;
   }
   createSpaces() {
-    console.log(this.spaces);
     let bonusSpaces = data.wheel.filter((spaces) => typeof spaces === 'number' )
     let increasedSpaces = bonusSpaces.map((space) => {
       return space*2;
     })
-    let randomMin = helper.getRandomInt(1, 17);
+    let randomMin = helper.getRandomInt(1, 13);
     let randomMax = randomMin + 6;
     let newSpaces = increasedSpaces.slice(randomMin, randomMax);
-    console.log(newSpaces);
     this.spaces.push(...newSpaces)
   }
 }
